9d538943f1a734dedfc359929e2a06c275db8ba2
Erin Scully <erin3scully@icloud.com>
2020-11-27 22:28:14 -0700

    Initial project commit

---
+++ Spotify Clone/VIEW/AlbumCell.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class AlbumCell: UICollectionViewCell {
+    
+    @IBOutlet weak var albumArtwork: UIImageView!
+    
+    @IBOutlet weak var albumTitleLabel: UILabel!
+    
+    @IBOutlet weak var artistNameLabel: UILabel!
+}
---
+++ Spotify Clone/VIEW/CategoryCell.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryCell.swift
+//  Spotify Clone
+//
+//  Created by Erin Scully on 11/27/20.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categorySubtitleLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+
+ 
+    
+    
+}
