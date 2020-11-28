0c34dd10cf547b5d6334b8644376ced053ea315a
Erin Scully <erin3scully@icloud.com>
2020-11-28 00:03:12 -0700

    Album and Song View Controllers created and auto-layout constraints added

---
+++ Spotify Clone/CONTROLLER/RootViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spotify Clone
+//
+//  Created by Erin Scully on 11/27/20.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+
+    
+
+
+
+
+
+
